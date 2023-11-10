a0ae169ebb3d29f139a9f59fa0020f80c3c7d539
Chris <91094210+chris-stytch@users.noreply.github.com>
2023-11-09 17:04:00 -0800

    Team hint

--- src/components/LoginWithSlack.tsx
+++ src/components/LoginWithSlack.tsx
@@ -7,7 +7,7 @@ const LoginWithSlack = () => {
   return (
     <div className="card">
       <h1>Login or signup</h1>
-        <button className="primary"  onClick={() => window.location.href="https://test.stytch.com/v1/public/oauth/slack/start?public_token=public-token-test-d9b9278d-426f-4f75-8ff4-ca09bc284df0&provider_team=T02EGRUMRM1"}>
+        <button className="primary"  onClick={() => window.location.href="https://test.stytch.com/v1/public/oauth/slack/start?public_token=public-token-test-d9b9278d-426f-4f75-8ff4-ca09bc284df0&provider_team=T015N08QWLC"}>
         Log in with Slack
         </button>
     </div>
