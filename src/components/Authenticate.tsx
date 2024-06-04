3b2dd9410d57fc4554680c7e1f8c68040499fec0
Pei-Yi Lin <plin@stytch.com>
2024-06-04 10:18:26 -0700

    Remove OAuth and OTP

--- src/components/Authenticate.tsx
+++ src/components/Authenticate.tsx
@@ -4,16 +4,15 @@ import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useStytchUser, useStytch } from "@stytch/nextjs";
 
-const OAUTH_TOKEN = "oauth";
 const MAGIC_LINKS_TOKEN = "magic_links";
 
 /**
- * During both the Magic link and OAuth flows, Stytch will redirect the user back to your application to a specified redirect URL (see Login.tsx).
+ * During the Magic link flow, Stytch will redirect the user back to your application to a specified redirect URL (see Login.tsx).
  * Stytch will append query parameters to the redirect URL which are then used to complete the authentication flow.
  * A redirect URL for this example app will look something like: http://localhost:3000/authenticate?stytch_token_type=magic_links&token=abc123
  *
  * The AuthenticatePage will detect the presence of a token in the query parameters, and attempt to authenticate it.
- * 
+ *
  * On successful authentication, a session will be created and the user will be redirect to /profile.
  */
 const Authenticate = () => {
@@ -27,11 +26,7 @@ const Authenticate = () => {
       const token = searchParams.get("token");
       const stytch_token_type = searchParams.get("stytch_token_type");
 
-      if (token && stytch_token_type === OAUTH_TOKEN) {
-        stytch.oauth.authenticate(token, {
-          session_duration_minutes: 60,
-        });
-      } else if (token && stytch_token_type === MAGIC_LINKS_TOKEN) {
+      if (token && stytch_token_type === MAGIC_LINKS_TOKEN) {
         stytch.magicLinks.authenticate(token, {
           session_duration_minutes: 60,
         });
